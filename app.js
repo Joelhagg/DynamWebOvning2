699a917ccecc794b294f6947c2773a15fc6035bd
joelhagg <joel.pettersson90@gmail.com>
2022-05-04 16:13:54 +0200

    update app.js add book func

--- app.js
+++ app.js
@@ -17,8 +17,44 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-app.get("/books", function (req, res) {
-  res.send("Hej till books routern!");
+app.get("/books/:bookId", function (req, res) {
+  let showBook = req.params.bookId;
+  res.send("Hej till books routern! Visa info om bok nummer " + showBook);
+});
+
+app.get("/buss/:from/:to", function (req, res) {
+  res.send(
+    "visar bussresor från " + req.params.from + " till " + req.params.to + "."
+  );
+});
+
+app.get("/addBook", (req, res) => {
+  let printForm = `<h1>Lägg till bok</h1>
+     <form action="saveBook" method="post">Vill du lägga till en bok?<br><br>
+      <input type="text" name="title" placeholder="Titel"/><br>
+      <input type="text" name="author" placeholder="Författare"/><br>
+      <input type="number" name="pages"placeholder="Antal sidor"/><br><br>
+      <button>Lägg till bok</button>
+     </form>`;
+
+  res.send(printForm);
+});
+
+let books = [];
+
+app.post("/saveBook", (req, res) => {
+  let exampleBooks = [{ title: "Bok1", author: "Författare1", pages: "123" }];
+  let addedBook = [
+    { title: req.body.title, author: req.body.author, pages: req.body.pages },
+  ];
+
+  if (books.length == 0) {
+    books.push(exampleBooks);
+    books.push(addedBook);
+  } else {
+    books.push(addedBook);
+  }
+  res.json(books);
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("users root");
 });
+
+router.get("/newUser", function (req, res) {
+  res.send("new user router");
+});
+
+router.post("/newUser", function (req, res) {});
 
 module.exports = router;
